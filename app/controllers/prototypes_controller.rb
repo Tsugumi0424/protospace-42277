b0127113e941e8a97efe413b69d487e0f25be8ef
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-01 04:13:21 +0900

    fifth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:new, :edit, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def move_to_edit
     unless user_signed_in?
