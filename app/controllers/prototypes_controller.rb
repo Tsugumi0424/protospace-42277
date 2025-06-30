6ac8930a00b13d76c44c15841c2a1fd783ac40f2
Tsugumi0424 <griveofficial0312@gmail.com>
2025-07-01 05:58:46 +0900

    tenth commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,5 +1,5 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def move_to_edit
     unless user_signed_in?
